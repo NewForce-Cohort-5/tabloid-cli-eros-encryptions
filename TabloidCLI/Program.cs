f2be4538ea1be691beed19509ebc2cb322fee5c6
jonahamoore <jonahamoore@gmail.com>
2022-02-17 10:21:15 -0500

    pushing

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -7,42 +7,34 @@ namespace TabloidCLI
     {
         static void Main(string[] args)
         {
-            //this is a place holder writeline for nicks thing
-            //Console.WriteLine("Welcome to Tabloid 720! This program does things, sometimes!");
             Console.WriteLine("---------------------");
             Console.WriteLine("Choose a background color, press 0 to leave as is");
             //background colors
 
-            //stack overflow code
-            //Array marks = Enum.GetValues(typeof(Mark));
-            //foreach (var mark in marks)
+            
+            //loop here
+
+            //string choice = Console.ReadLine();
+            //switch (choice)
             //{
-            // Console.WriteLine(mark);
-            // Console.BackgroundColor = ConsoleColor.Yellow;
+            //    case "0":
+            //        break;
+            //    case "1":
+            //        Console.BackgroundColor = ConsoleColor.Blue;
+            //        Console.Clear();
+            //    case "2":
+            //        Console.BackgroundColor = ConsoleColor.Green;
+            //        Console.Clear();
+            //    case "3":
+            //        Console.BackgroundColor = ConsoleColor.Red;
+            //        Console.Clear();
+            //    case "4":
+            //        Console.BackgroundColor = ConsoleColor.Yellow;
+            //        Console.Clear();
+            //    case "5":
+            //        Console.BackgroundColor = ConsoleColor.White;
+            //        Console.Clear();
             //}
-
-
-            string choice = Console.ReadLine();
-            switch (choice)
-            {
-                case "0":
-                    break;
-                case "1":
-                    Console.BackgroundColor = ConsoleColor.Blue;
-                    Console.Clear();
-                case "2":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.Clear();
-                case "3":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.Clear();
-                case "4":
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.Clear();
-                case "5":
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.Clear();
-            }
 
             // MainMenuManager implements the IUserInterfaceManager interface
             IUserInterfaceManager ui = new MainMenuManager();
