0ac13d68c8eedf858be5a70d65b8285febde746b
jonahamoore <jonahamoore@gmail.com>
2022-02-17 14:33:41 -0500

    fixed createdatetime in note

--- TabloidCLI/UserInterfaceManagers/NoteManager.cs
+++ TabloidCLI/UserInterfaceManagers/NoteManager.cs
@@ -56,6 +56,7 @@ namespace TabloidCLI.UserInterfaceManagers
             {
                 Console.WriteLine(note.Title);
                 Console.WriteLine(note.Content);
+                Console.WriteLine($"Date:{note.CreateDateTime}");
                 Console.WriteLine("-------------------");
             }
         }
